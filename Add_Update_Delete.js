c4ad759530a436a9944bacb44c370b6fcad5085c
Nishant Kumar <nishant201kashyap@gmail.com>
2025-06-21 19:00:19 +0530

    Add files via upload

---
+++ Add_Update_Delete.js
@@ -0,0 +1,10 @@
+let student = {
+    Name : ["Nishant","Prashant","Rishant","Alok"],
+    RollNo : ["23","12","1","98"],
+    City : ["New Delhi","Mumbai", "Patna","Kashmir"],
+    Height : 12
+}
+
+student.gender = ["Male","Male","Male","Male"];
+delete student.Height;
+student.Name[4]="Rahika";

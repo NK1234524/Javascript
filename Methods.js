b42539112dd02f34e44c6f26f1a3e997f99c10f9
Nishant Kumar <nishant201kashyap@gmail.com>
2025-06-22 18:14:52 +0530

    Add files via upload

---
+++ Methods.js
@@ -0,0 +1,9 @@
+const calculator  =  {
+
+    add : function(a,b){//Method of add
+        return a+b;
+    },
+    subtract : function(a,b){//Method of substract
+        return a-b;
+    }
+}
---
+++ RollDice.js
@@ -0,0 +1,5 @@
+function Roll_the_dice(){
+const roll = 6;
+let random = Math.floor(Math.random()*roll+1);
+console.log(`Random number on the dice is ${random}`);
+}

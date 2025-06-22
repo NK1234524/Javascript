43442367fdbe2cb87321a484e5de549d6052c17d
Nishant Kumar <nishant201kashyap@gmail.com>
2025-06-22 17:46:33 +0530

    Add files via upload

---
+++ Function.js
@@ -0,0 +1,20 @@
+function hello(){
+    console.log("Hello");
+}
+function work(){
+    console.log("Work together");
+}
+
+
+function Marks(Name){
+    const students = {
+        Nishant :{
+            Mark : 30
+        },
+        Prashant :{
+            Mark : 40
+        }
+    }
+    console.log(`Marks of ${Name} is ${students.Nishant.Mark}`);
+}
+

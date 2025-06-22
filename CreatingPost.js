532aa3241daf732a4c08708ccb1c51580bdbc173
Nishant Kumar <nishant201kashyap@gmail.com>
2025-06-22 17:56:10 +0530

    Revert "Guessing game.js"

--- CreatingPost.js
+++
@@ -1,7 +0,0 @@
-let Post={
-    Username : "Nishant21kashyap",
-    Id : "nishant201kashyap@gmail.com",
-    Content : "beautiful",
-    Date : "21/06/25",
-    Likes : "15M"
-};
--- Random_Integers.js
+++
@@ -1,2 +0,0 @@
-Math.floor(Math.random()*100)+1;
-//For random number among 100 from 1 

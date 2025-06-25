4f6209a4d9f663a0dfee88a54712703db875dd10
Nishant Kumar <nishant201kashyap@gmail.com>
2025-06-25 11:24:57 +0530

    Add files via upload

---
+++ goodstring.js
@@ -0,0 +1,12 @@
+let string= "apple";
+if((string[0]==='a') && (string.length>3)){
+    console.log("Good string");
+}
+else{
+console.log("Not a good string");
+}
+alert("Something went wrong");
+console.error("Wrong");//For error return on console 
+console.warn("Please move forward");//For warning sign return on the console.
+let nameee = prompt("Enter the value of name ");
+console.log(nameee);

ed9494df0af9150d2a77d63c904853b69f3f7ba8
Nishant Kumar <nishant201kashyap@gmail.com>
2025-06-25 11:25:32 +0530

    Add files via upload

---
+++ Trafficlight.js
@@ -0,0 +1,12 @@
+let color="red";
+let newcolor="yellow";
+let thirdcolor= "green";
+if(color=="red"){
+    console.log("STOP");
+}
+else if(color=="yellow"){
+    console.log("Move slowly");
+}
+else{
+    console.log("Move");
+}

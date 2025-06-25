4e51b86861964fd6e775f070bb55b672650220a8
Nishant Kumar <nishant201kashyap@gmail.com>
2025-06-25 21:57:35 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+// let count = document.getElementsByClassName("oldImg");
+// for(let i =0 ; i<count.length ; i++){
+//     count[i].src="spiderman_img.png";
+//     console.dir(`The image source is changed  ${i}  `)    ;
+// }
